b918de229ec34d57301915d325ae332347bdbcf9
Tundephilps <tundephilps@gmail.com>
2023-12-23 20:32:24 +0100

    Recover Screen

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
+import Recover from "./screens/Recover";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <SignUp />
+      {/* <SignUp /> */}
       {/* <Login /> */}
+      <Recover />
     </View>
   );
 }
--- screens/Recover.jsx
+++ screens/Recover.jsx
@@ -1,10 +1,99 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 
 const Recover = () => {
+  const [email, setEmail] = useState("");
+
+  //   const handleResetPassword = async () => {
+  //     try {
+  //       // Call your backend API to reset the password
+  //       // Replace 'YOUR_BACKEND_API_ENDPOINT' with the actual endpoint
+  //       const response = await fetch("YOUR_BACKEND_API_ENDPOINT/reset-password", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ email }),
+  //       });
+
+  //       if (response.ok) {
+  //         // Password reset successful
+  //         Alert.alert(
+  //           "Password Reset",
+  //           "Check your email for further instructions."
+  //         );
+  //       } else {
+  //         // Password reset failed
+  //         Alert.alert("Password Reset Failed", "Please try again later.");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error resetting password:", error);
+  //       Alert.alert("Error", "An unexpected error occurred. Please try again.");
+  //     }
+  //   };
   return (
     <View style={styles.container}>
-      <Text>Recover</Text>
+      <Text style={styles.logIn}>Recover</Text>
+      <View style={{ height: 58 }} />
+      <View style={{ width: "100%", alignSelf: "center" }}>
+        <Text style={{ fontSize: 16, color: "#030229", marginBottom: 8 }}>
+          Email Address
+        </Text>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="email-address"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          placeholder="example@gmail.com"
+          placeholderTextColor="#6b7280"
+          style={{
+            height: 50,
+            borderColor: "lightgray",
+            borderRadius: 5,
+            paddingHorizontal: 12,
+            backgroundColor: "#F7F7F8",
+          }}
+        />
+      </View>
+
+      <View style={{ height: 26 }} />
+      <View
+        style={{
+          width: "100%",
+        }}
+      >
+        <TouchableOpacity
+
+        //onPress={handleResetPassword}
+        >
+          <View
+            style={{
+              backgroundColor: "#B847EF", // Background color
+              height: 50,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16, // Font size
+                fontWeight: "normal", // Font weight
+                color: "#fff", // Text color
+              }}
+            >
+              Reset Your Password
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -15,7 +104,14 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     height: "100%",
-    width: "100%",
     justifyContent: "center",
+    alignItems: "center",
+    padding: 12,
+  },
+  logIn: {
+    fontSize: 25,
+    fontWeight: "600",
+    color: "#000",
+    alignSelf: "center",
   },
 });
